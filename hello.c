13dea175519df5b06fd0281e19d423069814b4fc
tsallinger1993 <42591399+tsallinger1993@users.noreply.github.com>
2018-08-23 16:23:15 -0500

    I entered my Major

--- hello.c
+++ hello.c
@@ -1,5 +1,5 @@
 /**
- * Author: Kenneth
+ * Author: Kenneth, Anthony, Max
  * Date: 2018/08/23
  *
  * A simple hello world program in C
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv) {
 
-  printf("Kenneth Thomason, Electrical Engineering! Max Einfalt, Computer Engineering!\n");
+  printf("Kenneth Thomason, Electrical Engineering! Max Einfalt, Computer Engineering! Anthony Sallinger, Computer Science!\n");
 
   return 0;
 }
