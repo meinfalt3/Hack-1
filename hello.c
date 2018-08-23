045eea779d9a5c5144040eacbaba28f9af09d45f
KennyThomason <42561699+KennyThomason@users.noreply.github.com>
2018-08-23 16:09:53 -0500

    Added Hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Kenneth
+ * Date: 2018/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Kenneth!\n");
+
+  return 0;
+}
